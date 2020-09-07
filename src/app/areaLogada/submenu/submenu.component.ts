e841c19ec4eebac39a0a4c24b2d34cad93c2f53c
valdney89 <valdneysouza@gmail.com>
2020-09-07 01:26:00 -0300

    3 files modified, 6 added

---
+++ src/app/areaLogada/submenu/submenu.component.ts
@@ -0,0 +1,9 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'submenu',
+  templateUrl: './submenu.component.html',
+  styleUrls: [ './submenu.component.css' ]
+})
+export class SubMenuComponent  {
+}
