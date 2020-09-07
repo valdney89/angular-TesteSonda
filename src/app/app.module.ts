ea111a6d4c4005485639aff41ce017102eefb894
valdney89 <valdneysouza@gmail.com>
2020-09-07 01:59:38 -0300

    4 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './areaLogada/home/home.component';
 import { MenuEsquerdoComponent } from './areaLogada/menuEsquerdo/menuEsquerdo.component';
 import { SubMenuComponent } from './areaLogada/submenu/submenu.component';
+import { BlocoTituloComponent } from './areaLogada/blocoTitulo/blocoTitulo.components';
 
 const routes: Routes = [
     { path: 'login', component: LoginComponent },
@@ -17,7 +18,7 @@ const routes: Routes = [
 
 @NgModule({
   imports:      [ BrowserModule, RouterModule.forRoot(routes) ],
-  declarations: [ AppComponent, HeaderComponent, HomeComponent, MenuEsquerdoComponent, SubMenuComponent ],
+  declarations: [ AppComponent, HeaderComponent, HomeComponent, MenuEsquerdoComponent, SubMenuComponent, BlocoTituloComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/areaLogada/blocoTitulo/blocoTitulo.components.ts
@@ -0,0 +1,9 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'bloco-titulo',
+  templateUrl: './blocoTitulo.component.html',
+  styleUrls: [ './blocoTitulo.component.css' ]
+})
+export class BlocoTituloComponent  {
+}
