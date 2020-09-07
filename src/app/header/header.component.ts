81155a72c033a1936fb16ac9cc5a6a2869b522a5
valdney89 <valdneysouza@gmail.com>
2020-09-06 23:59:07 -0300

    3 files modified

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,4 +6,7 @@ import { Component, Input } from '@angular/core';
   styleUrls: [ './header.component.css' ]
 })
 export class HeaderComponent  {
+
+  logoEmpresa = `https://cdn.jsdelivr.net/gh/valdney89/angular-TesteSonda@master/src/assets/imgs/Ativo%201.png`;
+  altLogoEmpresa = `Empresa ABC`;
 }
